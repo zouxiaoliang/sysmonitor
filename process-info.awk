07e70e1f28d6236a31679050eff2fa7646af0c65
zouxiaoliang <295919168@qq.com>
2021-07-21 17:15:33 +0800

    优化 1、进程解析相关移动到awk中实现

--- process-info.awk
+++ process-info.awk
@@ -4,11 +4,11 @@
     res = $5
     vsz = $6
     cmdline = ""
-    for (i = 11 ; i < NF; ++i) {
+    for (i = 11 ; i <= NF; ++i) {
         if (length(cmdline) > 0) {
             cmdline = cmdline " "
         }
         cmdline = cmdline $i
     }
-    print "cpu: " cpu ", mem: " mem ", res: " res ", vsz: " vsz ", cmdline: \"" cmdline "\""
+    print "name: " name ", pid: " pid ", cpu: " cpu ", mem: " mem ", res: " res ", vsz: " vsz ", cmdline: \"" cmdline "\""
 }
--- process-monitor.sh
+++ process-monitor.sh
@@ -28,14 +28,16 @@ function print_slab() {
 }
 
 function print_process_info() {
-  PIDS=$(ps -ef | grep $1 | grep -v 'grep' | grep -v "${MYNAME}" | awk '{print $2}')
+  PIDS=$(ps -ef | grep $1 | grep -v 'grep' | grep -v "${MYNAME}" | grep -v 'awk -f process-info.awk' | awk '{print $2}')
   OLD_IFS="$IFS"
   IFS=","
   array=($PIDS)
   IFS="$OLD_IFS"
   for pid in ${array[@]}; do
-    process_info=$(ps aux | grep $pid | grep -v 'grep' | awk -f process-info.awk)
-    echo "NAME: ${1} PID: ${pid} ${process_info}"
+    process_info=$(ps aux | grep " $pid " | grep -v 'grep')
+    # echo "-- $process_info"
+    process_info=`echo $process_info | awk -f process-info.awk -v pid=$pid name=$1`
+    echo "${process_info}"
   done
 }
 
@@ -56,10 +58,10 @@ function main() {
     done
     echo
 
-    print_memory
-    print_disk
-    print_slab
-
+    # print_memory
+    # print_disk
+    # print_slab
+	break
     sleep ${CYCLE}
   done
 }
